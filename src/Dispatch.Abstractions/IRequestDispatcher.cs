113fb0c0ec822b6110a2aa2edde906664befa0cf
Nightowl <79215414+nightowl286@users.noreply.github.com>
2022-12-03 00:00:07 +0000

    Documentation pass: Finished documenting the abstractions assembly.

--- src/Dispatch.Abstractions/IRequestDispatcher.cs
+++ src/Dispatch.Abstractions/IRequestDispatcher.cs
@@ -4,14 +4,41 @@ using TNO.Dispatch.Abstractions.Results;
 
 namespace TNO.Dispatch.Abstractions
 {
+   /// <summary>
+   /// Denotes a common request dispatcher.
+   /// </summary>
+   /// <typeparam name="TRequestConstraint">
+   /// The type of <see cref="IDispatchRequest"/> that this dispatcher will allow.
+   /// </typeparam>
    public interface IRequestDispatcher<in TRequestConstraint>
       where TRequestConstraint : notnull, IDispatchRequest
    {
       #region Methods
+      /// <summary>
+      /// Checks whether the <typeparamref name="TOutput"/>/<typeparamref name="TRequest"/>
+      /// combination can be dispatched.
+      /// </summary>
+      /// <typeparam name="TOutput">The expected output type.</typeparam>
+      /// <typeparam name="TRequest">The type of the request.</typeparam>
+      /// <returns>
+      /// <see langword="true"/> if the <typeparamref name="TOutput"/>/<typeparamref name="TRequest"/>
+      /// combination can be dispatched, <see langword="false"/> otherwise.
+      /// </returns>
       bool CanDispatch<TOutput, TRequest>()
          where TOutput : notnull
          where TRequest : TRequestConstraint;
 
+      /// <summary>
+      /// Asynchronously dispatches the given <paramref name="request"/> with an 
+      /// expected output of the type <typeparamref name="TOutput"/>.
+      /// </summary>
+      /// <typeparam name="TOutput">The expected output type.</typeparam>
+      /// <typeparam name="TRequest">The type of the <paramref name="request"/>.</typeparam>
+      /// <param name="request">The request to dispatch.</param>
+      /// <param name="cancellationToken">
+      /// A <see cref="CancellationToken"/> that can be used to cancel this operation.
+      /// </param>
+      /// <returns>The dispatch result returned by the handler.</returns>
       ValueTask<IDispatchResult<TOutput>> DispatchAsync<TOutput, TRequest>(TRequest request, CancellationToken cancellationToken = default)
          where TOutput : notnull
          where TRequest : TRequestConstraint;
--- src/Dispatch.Abstractions/IRequestRegistrar.cs
+++ src/Dispatch.Abstractions/IRequestRegistrar.cs
@@ -10,10 +10,49 @@ namespace TNO.Dispatch.Abstractions
    public interface IRequestRegistrar<out TCollection>
    {
       #region Methods
+      /// <summary>
+      /// Registers the given <paramref name="handlerType"/> with the given
+      /// <paramref name="outputType"/>/<paramref name="requestType"/> combination.
+      /// </summary>
+      /// <param name="outputType">The type of the output the handler will return.</param>
+      /// <param name="requestType">The type of the request the handler can handle.</param>
+      /// <param name="handlerType">
+      /// The type of the handler to register with the given
+      /// <paramref name="outputType"/>/<paramref name="requestType"/> combination.
+      /// </param>
       void Register(Type outputType, Type requestType, Type handlerType);
+
+      /// <summary>
+      /// Registers the given <paramref name="handlerType"/> for
+      /// all <see cref="IRequestHandler{TOutput, TRequest}"/> 
+      /// that the <paramref name="handlerType"/> implements.
+      /// </summary>
+      /// <param name="handlerType">The type of the handler to register.</param>
       void Register(Type handlerType);
+
+      /// <summary>
+      /// Registers the given <paramref name="handlerType"/> with the given workflow and
+      /// <paramref name="outputType"/>/<paramref name="requestType"/> combination.
+      /// </summary>
+      /// <param name="outputType">The type of the output the handler will return.</param>
+      /// <param name="requestType">The type of the request the handler can handle.</param>
+      /// <param name="handlerType">
+      /// The type of the handler to register with the given workflow and
+      /// <paramref name="outputType"/>/<paramref name="requestType"/> combination.
+      /// </param>
+      /// <param name="workflow">The workflow to use when registering the <paramref name="handlerType"/>.</param>
       void Register(Type outputType, Type requestType, Type handlerType, IDispatchWorkflow workflow);
+
+      /// <summary>
+      /// Registers the given <paramref name="handlerType"/> (with the given <paramref name="workflow"/>) 
+      /// for all <see cref="IRequestHandler{TOutput, TRequest}"/> that the <paramref name="handlerType"/> implements.
+      /// </summary>
+      /// <param name="handlerType">The type of the handler to register.</param>
+      /// <param name="workflow">The workflow to use when registering the <paramref name="handlerType"/>.</param>
       void Register(Type handlerType, IDispatchWorkflow workflow);
+
+      /// <summary>Creates a new scope for the current collection.</summary>
+      /// <returns>The newly created scope.</returns>
       TCollection CreateScope();
       #endregion
    }
